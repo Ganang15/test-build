e6e81cf15935fd93b61bda90cd0133982dfba382
Bimo <bimo1288@gmail.com>
2023-01-27 20:29:17 +0700

    testing

---
+++ client/build/precache-manifest.256b1d189f347bfb38404db3fa6014df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f535ec9f965caed82b489ea8e8065fb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15dad8100a173015bb54",
+    "url": "/static/css/main.d3f13e23.chunk.css"
+  },
+  {
+    "revision": "0ee4f30bdde23470fefb",
+    "url": "/static/js/2.011c3ce8.chunk.js"
+  },
+  {
+    "revision": "038db422c0414d8dfd31541a1bf2a15e",
+    "url": "/static/js/2.011c3ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15dad8100a173015bb54",
+    "url": "/static/js/main.82ddc730.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.82ddc730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
